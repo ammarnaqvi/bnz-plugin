5ebce61edad62de47cf7d775e14e0f4e71519bee
rehanalam <m.rehan.alam@gmail.com>
2021-06-06 12:23:39 +0500

    Implements BNZ demo app auth flow

--- src/App.js
+++ src/App.js
@@ -7,26 +7,29 @@ import {
   Input,
   Button,
   Radio,
+  Alert
 } from 'antd';
+import bnzSdk from 'bnzapimatic';
+import { Base64 as base64 } from 'js-base64';
 
 // const bnzSdk = require('APIMatic+Ltd+Bank+Feeds+Sandbox-NodeJs');
 
 
 const POLYFILL_URL =
-    // tslint:disable-next-line:max-line-length
-    'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,Array.prototype.findIndex,Array.prototype.@@iterator&excludes=Promise,fetch';
+  // tslint:disable-next-line:max-line-length
+  'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.find,Array.prototype.findIndex,Array.prototype.@@iterator&excludes=Promise,fetch';
 
 const bNZPluginInterface = {
-    // tslint:disable-next-line:no-any
-    show(settings) {
-        loadScript(POLYFILL_URL, () => {
-            localStorage.setItem('portalEditorSettings', JSON.stringify(settings));
-            ReactDOM.render(
-                <App settings={settings} />,
-                document.getElementById('root')
-            );
-        });
-    },
+  // tslint:disable-next-line:no-any
+  show(settings) {
+    loadScript(POLYFILL_URL, () => {
+      localStorage.setItem('portalEditorSettings', JSON.stringify(settings));
+      ReactDOM.render(
+        <App settings={settings} />,
+        document.getElementById('root')
+      );
+    });
+  },
 };
 
 function loadScript(url, callback) {
@@ -36,7 +39,7 @@ function loadScript(url, callback) {
 
   scriptTag.onload = callback;
   scriptTag.onerror = () => {
-      throw new Error('APIMatic: Failed to load ' + url);
+    throw new Error('APIMatic: Failed to load ' + url);
   };
 
   document.getElementsByTagName('head')[0].appendChild(scriptTag);
@@ -47,48 +50,115 @@ window.BNZPlugin = bNZPluginInterface;
 
 
 
-function App () {
+function App() {
 
-  const [OAuthClientId, setOAuthClientId] = useState("OAuthClientId")
-  const [OAuthClientSecret, setOAuthClientSecret] = useState("OAuthClientSecret")
-  const [OAuthRedirectUri, setOAuthRedirectUri] = useState("OAuthRedirectUri")
+  const [OAuthClientId, setOAuthClientId] = useState("1U5hCE3h3AhCJXGNgOstGp6drkaMxB4S")
+  const [OAuthClientSecret, setOAuthClientSecret] = useState("fpiG05c6tEjwkPmr")
+  const [OAuthRedirectUri, setOAuthRedirectUri] = useState('https://apimatic-proxy.azurewebsites.net/oauth2_callback')
+  const [isAuthorized, setIsAuthorized] = useState(false);
 
-  // const configuration = bnzSdk.Configuration;
+  let state = `https://apimatic-proxy.azurewebsites.net/oauth2_callback;${OAuthClientId};${OAuthClientSecret};https://secure.sandbox.bnz.co.nz/pingfederate/as/token.oauth2;${window.location.origin}`;
 
-  // const oAuthManager = bnzSdk.OAuthManager;
-  // configuration.oAuthClientId = 'OAuthClientId';
-  // configuration.oAuthClientSecret = 'OAuthClientSecret';
-  // configuration.oAuthRedirectUri = 'OAuthRedirectUri';
+  let authorizeClient = () => {
+    console.log('hello')
+    const configuration = bnzSdk.Configuration;
 
-  // const authUrl = oAuthManager.buildAuthorizationUrl([bnzSdk.OAuthScopeEnum.BANKFEEDS]);
+    const oAuthManager = bnzSdk.OAuthManager;
+    configuration.oAuthClientId = OAuthClientId;
+    configuration.oAuthClientSecret = OAuthClientSecret;
+    configuration.oAuthRedirectUri = OAuthRedirectUri;
 
+    let authUrl = oAuthManager.buildAuthorizationUrl([bnzSdk.OAuthScopeEnum.BANKFEEDS], state);
+
+    let promise = new Promise(
+      (resolve, reject) => {
+        const newWindow = window.open(authUrl);
+        if (newWindow === null) {
+          reject();
+          return;
+        }
+        window.addEventListener(
+          'message',
+          event => {
+            if (
+              event.origin ===
+              new URL(
+                OAuthRedirectUri
+              ).origin
+            ) {
+              // resolved = true;
+              const decodedBody = base64.decode(event.data.RawContent);
+              resolve(JSON.parse(decodedBody));
+            }
+          },
+          false
+        );
+      }
+    );
+
+    let authTokenPromise = bnzSdk.OAuthManager.setOAuthToken(promise);
+    authTokenPromise.then(resp => {
+      console.log(bnzSdk.OAuthManager.isTokenSet());
+      setIsAuthorized(bnzSdk.OAuthManager.isTokenSet());
+    }).catch(err => {
+      console.log(err);
+    })
+
+    // let code = 'GRJ0_1a5MPwQsFW4NdkLNTFZiwtMyQ4Io5AAABkh';
+    // const tokenPromise = oAuthManager.authorize(code);
+    // tokenPromise.then(res => {
+    //   console.log(res);
+    // }).catch(err => {
+    //   console.log(err);
+    // })
+
+
+  }
+
+  let getV3Accounts = () => {
+    let account = bnzSdk.MiscController.gETv3Accounts('VD9ww2IxRnUWtTUYipeDx33GUTYHJRpn');
+
+    account.then(response => {
+      console.log(response)
+    }).catch(err => {
+      console.log(err);
+    })
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-      <Form
-        labelCol={{
-          span: 4,
-        }}
-        wrapperCol={{
-          span: 6,
-        }}
-        layout="horizontal"
-        size="large"
-      >
-        <Form.Item label="OAuthClientId">
-          <Input value={OAuthClientId} onChange={(e) => setOAuthClientId(e.target.value)} />
-        </Form.Item>
-        <Form.Item label="OAuthClientSecret">
-        <Input value={OAuthClientSecret} onChange={(e) => setOAuthClientSecret(e.target.value)} />
-        </Form.Item>
-        <Form.Item label="OAuthRedirectUri">
-        <Input value={OAuthRedirectUri} onChange={(e) => setOAuthRedirectUri(e.target.value)} />
-        </Form.Item>
-        <Form.Item label="Button">
-          <Button>Submit</Button>
-        </Form.Item>
-      </Form>
+        {isAuthorized && <Alert message="Authorized Successfully" type="success" showIcon />}
+
+        <Form
+          labelCol={{
+            span: 4,
+          }}
+          wrapperCol={{
+            span: 6,
+          }}
+          layout="horizontal"
+          size="large"
+        >
+          <Form.Item label="OAuthClientId">
+            <Input value={OAuthClientId} onChange={(e) => setOAuthClientId(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="OAuthClientSecret">
+            <Input value={OAuthClientSecret} onChange={(e) => setOAuthClientSecret(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="OAuthRedirectUri">
+            <Input value={OAuthRedirectUri} onChange={(e) => setOAuthRedirectUri(e.target.value)} />
+          </Form.Item>
+          <Form.Item label="Button">
+            <Button onClick={authorizeClient}>Submit</Button>
+          </Form.Item>
+          {isAuthorized && <p>Authorized Successfully</p>}
+        </Form>
+
+        <h2>Get Account</h2>
+        <Button type="primary" onClick={getV3Accounts}>Get V3 Account</Button>
+
+
       </header>
     </div>
   );
